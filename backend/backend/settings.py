def8344cef6dcb9dce24a8e943fd31a32a0dd15e
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-03-09 09:39:52 -0500

    set up keys

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+backend = os.path.expanduser('./backend')
+load_dotenv(os.path.join(backend, 'keys.py'))
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
