3693ffef309d6fcd801cefb3011331f18542028e
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-03-09 09:29:34 -0500

    add .gitignore

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u=mgrfoc)k^nnv%f=$fh&^4mk&icfqk9ggx*xa$k=^^)$z0s-c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
