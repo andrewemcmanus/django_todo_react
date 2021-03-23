4959931bd90941a67c1e09ff781e7321b477ce67
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-03-23 10:14:58 -0400

    fixed bootstrap & reactstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-const toDoItems = [
+const todoItems = [
   {
     id: 1,
     title: "Byteboard Assessment",
@@ -63,7 +63,7 @@ class App extends Component {
 
   renderItems = () => {
     const { viewCompleted } = this.state;
-    const newItems = this.state.todoList.filter((item) => item.completed == viewCompleted);
+    const newItems = this.state.todoList.filter((item) => item.completed === viewCompleted);
 
     return newItems.map((item) => (
       <li key={item.id} className="list-group-item d-flex justify-content-between align-items-center">
